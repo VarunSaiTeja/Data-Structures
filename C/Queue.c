7592497fb0ab54e6b822a36c99a05f3b7c11bd6e
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-12-13 14:17:44 +0530

    Intial Commit to Queue

---
+++ C/Queue.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define capacity 5
+
+int queue[capacity],rear=0,front=0;
+
+void insertItem();
+void deleteItem();
+void displayItems();
+int isFull();
+int isEmpty();
+
+int main(int argc, char const *argv[])
+{
+    int choice;
+QueueMenu:
+    system("cls");
+    printf("Queue Operations : \n\n");
+    printf("1. Insert an item\n");
+    printf("2. Delete an item\n");
+    printf("3. Display all items\n");
+    printf("4. Quit");
+    printf("\n\nChoice : ");
+    scanf("%d", &choice);
+    system("cls");
+
+    switch (choice)
+    {
+    case 1:
+        //insertItem();
+        break;
+    case 2:
+        //deleteItem();
+        break;
+    case 3:
+        //displayItems();
+        break;
+    case 4:
+        exit(0);
+    default:
+        break;
+    }
+
+    printf("\n\n");
+    system("pause");
+    goto QueueMenu;
+}

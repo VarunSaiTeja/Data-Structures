f2895d37a6c9caab629cb4867684985130b097bb
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2018-12-16 21:23:19 +0530

    insertNode is defined to Binary Search Tree

--- C/Binary Search Tree.c
+++ C/Binary Search Tree.c
@@ -10,6 +10,53 @@ struct node
 
 struct node *root = NULL;
 
+void insertNode()
+{
+    struct node *temp, *parent;
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter Data : ");
+    scanf("%d", &temp->data);
+    system("cls");
+    
+    temp->left = NULL;
+    temp->right = NULL;
+
+    if (root == NULL)
+    {
+        root = temp;
+    }
+    else
+    {
+        struct node *current;
+        current = root;
+
+        while (current)
+        {
+            parent = current;
+            if (temp->data > current->data)
+            {
+                current = current->right;
+            }
+            else
+            {
+                current = current->left;
+            }
+        }
+
+        if (temp->data > parent->data)
+        {
+            parent->right = temp;
+        }
+        else
+        {
+            parent->left = temp;
+        }
+    }
+
+    printf("Node added to Binary Search Tree");
+}
+
 int main(int argc, char const *argv[])
 {
     int choice;
@@ -24,7 +71,7 @@ BinarySearchTreeMenu:
     switch (choice)
     {
     case 1:
-        //insertNode();
+        insertNode();
         break;
     case 2:
         //deleteNode();

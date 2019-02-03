3979600592ded3f8e545c579bc9aa2e736cf441a
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-02-03 15:43:12 +0530

    linear search is defined to array

--- C/Array.c
+++ C/Array.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#define TRUE 1
+#define FALSE 0
 
 int get_choice()
 {
@@ -8,7 +10,8 @@ int get_choice()
     system("cls");
     printf("Array Operations\n\n");
     printf("1. Display Array Elements\n");
-    printf("2. Exit");
+    printf("2. Linear Search\n");
+    printf("3. Exit\n");
     printf("\nChoice : ");
     scanf("%d", &choice);
     return choice;
@@ -22,6 +25,19 @@ void display_array_elements(int *array, int array_elements_count)
     {
         printf("%d ", array[index]);
     }
+}
+
+int linear_search(int *array, int array_elements_count, int element, int *location)
+{
+    for (int index = 0; index < array_elements_count; index++)
+    {
+        if (element == array[index])
+        {
+            *location = index;
+            return TRUE;
+        }
+    }
+    return FALSE;
 }
 
 int main(int argc, char const *argv[])
@@ -45,7 +61,25 @@ array_operations:
     case 1:
         display_array_elements(array, array_elements_count);
         break;
-    case 0:
+    case 2:
+    {
+        int element, location;
+        system("cls");
+        printf("Enter array element to be deleted : ");
+        scanf("%d", &element);
+        if (linear_search(array, array_elements_count, element, &location))
+        {
+            system("cls");
+            printf("Element found at index location : %d", location);
+        }
+        else
+        {
+            printf("Element not found");
+        }
+
+        break;
+    }
+    case 3:
         exit(0);
         break;
     default:

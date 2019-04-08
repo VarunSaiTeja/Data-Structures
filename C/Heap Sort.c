cb24118c3084f9d84b2bd93ad9febf1a428136ea
VarunSaiTeja <35555010+VarunSaiTeja@users.noreply.github.com>
2019-04-08 15:48:17 +0530

    Sorted array added to Heap Sort

--- C/Heap Sort.c
+++ C/Heap Sort.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <windows.h>
 
-int array[10], n = 0;
+#define capacity 10
+
+int array[capacity], n = 0;
+int sorted[capacity], n_sort = 0;
 
 void add_element()
 {
@@ -27,12 +30,61 @@ void add_element()
     array[1] = element;
 }
 
+void delete_element()
+{
+    int deleting_item = array[1];
+    sorted[++n_sort] = deleting_item;
+    int last_item = array[n];
+    int loc = 1, left = 2, right = 3;
+    --n;
+
+    while (right <= n)
+    {
+        if (last_item >= array[left] && last_item >= array[right])
+        {
+            array[loc] = last_item;
+            return;
+        }
+
+        if (array[right] > array[left])
+        {
+            array[loc] = array[right];
+            loc = right;
+        }
+        else
+        {
+            array[loc] = array[left];
+            loc = left;
+        }
+
+        left = 2 * loc;
+        right = 2 * loc + 1;
+    }
+
+    if (left == n && last_item < array[left])
+    {
+        array[loc] = array[left];
+        loc = left;
+    }
+    array[loc] = last_item;
+}
+
 void display_elements()
 {
     printf("Elements are : ");
     for (int index = 1; index <= n; index++)
     {
         printf("%d ", array[index]);
+    }
+    printf("\n\n");
+}
+
+void display_sorted()
+{
+    printf("Sorted Elements are : ");
+    for (short int index = 1; index <= n_sort; index++)
+    {
+        printf("%d ", sorted[index]);
     }
     printf("\n\n");
 }
@@ -44,8 +96,9 @@ menu:
     system("cls");
     printf("1. Add Element\n");
     printf("2. Delete Element\n");
-    printf("3. Display\n");
-    printf("4. Exit\n");
+    printf("3. Display Heap\n");
+    printf("4. Display Sorted Array\n");
+    printf("5. Exit\n");
     printf("\nChoice : ");
     scanf("%d", &choice);
     system("cls");
@@ -58,12 +111,15 @@ menu:
         printf("Element Inserted\n");
         break;
     case 2:
-        //delete_element();
+        delete_element();
         break;
     case 3:
         display_elements();
         break;
     case 4:
+        display_sorted();
+        break;
+    case 5:
         exit(0);
     default:
         break;
